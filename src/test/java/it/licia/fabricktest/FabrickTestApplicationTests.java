bb0f2091aadb8603ffcc97516fb5b5c9824a6887
l120191 <licia.belli@gmail.com>
2023-10-02 13:51:45 +0200

    First upload of empty project

---
+++ src/test/java/it/licia/fabricktest/FabrickTestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.licia.fabricktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
